537367ce3d4b4c03645ef74d5ae3c8703a8d5cff
andresperes33 <andresperesdev@gmail.com>
2024-07-27 11:09:09 -0300

    projeto-pronto

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+let formulario = document.querySelector(".formulario")
+console.log(formulario)
+
+function aparecerformulario(){
+    formulario.style.left = "37%"
+
+}
+
+function desaparecerFormulario(){
+
+    formulario.style.left = "-37%"
+
+
+}
+
